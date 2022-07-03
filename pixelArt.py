d1d2f9fc7f293b0237b0c08e5fdb011ce622c132
h-good <hazen.goodyear@gmail.com>
2022-07-02 23:20:10 -0600

    working with single image

--- pixelArt.py
+++ pixelArt.py
@@ -1,104 +1,87 @@
 from PIL import Image
 #import numpy as np
-import board
-import neopixel
+#import board
+#import neopixel
+#from PIL import ImageDraw
+from rgbmatrix import RGBMatrix, RGBMatrixOptions
 import time
 import os
 import random
 
-LED_mat = neopixel.NeoPixel(board.D18, 576, auto_write = False, pixel_order="BRG", brightness = 0.25)
+# Configuration for the matrix
+options = RGBMatrixOptions()
+options.rows = 32
+options.cols = 64
+options.gpio_slowdown = 4
+# options.chain_length = 1
+# options.parallel = 1
+options.hardware_mapping = 'adafruit-hat'  # If you have an Adafruit HAT: 'adafruit-hat'
+
+matrix = RGBMatrix(options = options)
+
+I = Image.open("C19.JPG")
+newSize = [32, 64]
+I = I.resize(newSize)
+pix = I.load()
+for i in range(64):
+    for j in range(32):
+        matrix.SetPixel(i,j,pix[j,i][0],pix[j,i][1],pix[j,i][2])
+
+# matrix.SetPixel()
+                        
+# I.thumbnail((matrix.width, matrix.height))
+# I = Image.Image.rotate(I, 270)
+
+# new_size = (32, 64)
+# resize image
+# I2 = I.resize(new_size)
+
+
+
+# matrix.SetImage(I.convert('RGB'))
+
 
 try:
-    while(1):
-        I_str = random.choice(os.listdir("/home/pi/Documents/Python/pixel_art_pics"))
-        # Begin if animated
-        if I_str.find('@') != -1:
-            endIndex = I_str.find('@')
-            base_str = I_str[0:endIndex:1]
-            animation1 = "/home/pi/Documents/Python/pixel_art_pics/" + base_str + "1@.JPG"
-            animation2 = "/home/pi/Documents/Python/pixel_art_pics/" + base_str + "2@.JPG"
-            for l in range(10):
-                if l % 2 == 0:
-                    I = Image.open(animation1)
-                else:
-                    I = Image.open(animation2)
-                new_size = (18, 32)
-                # resize image
-                I2 = I.resize(new_size)
-                pixels = I2.load()
-                try:
-                    j = 0
-                    for i in range(32):
-                        if i % 2 == 0:
-                            try:
-                                for k in range(18):
-                                    if k == 0:
-                                        LED_mat[j] = pixels[k, i]
-                                    else:
-                                        LED_mat[j] = pixels[k, i]
-                                    j = j + 1
-                            except:
-                                pass
-                        if i % 2 == 1:
-                            try:
-                                for l in range(18):
-                                    if j % 18 == 17:
-                                        pass
-                                    else:
-                                        LED_mat[j] = pixels[18-3-l, i]
-                                    j = j + 1
-                            except:
-                                pass
-                except:
-                    pass
-                LED_mat.show()
-                time.sleep(.25)
-#End if animated
-                            
-        I = Image.open("/home/pi/Documents/Python/pixel_art_pics/" +I_str)
-        
-
-        # Maintain aspect ratio while also maximizing use of display
-#         w_org, h_org = I.size
-#         w_percent = 18/w_org
-#         h_percent = 32/h_org
-
-        #Scale by the smaller of the percentages
-#         scale_percent = min(w_percent, h_percent)
-#         w_new = int(scale_percent*w_org)
-#         h_new = int(scale_percent*h_org)
-
-#         new_size = (w_new, h_new)
-        new_size = (18, 32)
-        # resize image
-        I2 = I.resize(new_size)
-        pixels = I2.load()
-        try:
-            j = 0
-            for i in range(32):
-                if i % 2 == 0:
-                    try:
-                        for k in range(18):
-                            if k == 0:
-                                LED_mat[j] = pixels[k, i]
-                            else:
-                                LED_mat[j] = pixels[k, i]
-                            j = j + 1
-                    except:
-                        pass
-                if i % 2 == 1:
-                    try:
-                        for l in range(18):
-                            if j % 18 == 17:
-                                pass
-                            else:
-                                LED_mat[j] = pixels[18-3-l, i]
-                            j = j + 1
-                    except:
-                        pass
-        except:
-            pass
-        LED_mat.show()
-        time.sleep(4)
+    print("Press CTRL-C to stop.")
+    while True:
+        time.sleep(100)
 except KeyboardInterrupt:
-    pass
+    sys.exit(0)
+
+#                             
+#         I = Image.open("/home/pi/Documents/Python/pixel_art_pics/" +I_str)
+#         
+#         new_size = (18, 32)
+#         # resize image
+#         I2 = I.resize(new_size)
+#         pixels = I2.load()
+#         try:
+#             j = 0
+#             for i in range(32):
+#                 if i % 2 == 0:
+#                     try:
+#                         for k in range(18):
+#                             if k == 0:
+#                                 LED_mat[j] = pixels[k, i]
+#                             else:
+#                                 LED_mat[j] = pixels[k, i]
+#                             j = j + 1
+#                     except:
+#                         pass
+#                 if i % 2 == 1:
+#                     try:
+#                         for l in range(18):
+#                             if j % 18 == 17:
+#                                 pass
+#                             else:
+#                                 LED_mat[j] = pixels[18-3-l, i]
+#                             j = j + 1
+#                     except:
+#                         pass
+#         except:
+#             pass
+#         LED_mat.show()
+#         time.sleep(4)
+# except KeyboardInterrupt:
+#     pass
+# 
